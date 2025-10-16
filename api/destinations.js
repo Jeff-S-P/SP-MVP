a7cca08c9b0e16db034b61c3b2e60038c1f5c5d7
Jeff-S-P <jefferson.e.spratt@gmail.com>
2025-10-17 07:31:28 +1100

    Update destinations.js

--- api/destinations.js
+++ api/destinations.js
@@ -4,10 +4,10 @@
 
 export default async function handler(req, res) {
   // Set CORS headers to allow requests from anywhere
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Content-Type', 'application/json');
 
   // Handle OPTIONS request (CORS preflight)
   if (req.method === 'OPTIONS') {
